5a1cead6c52e90573d20dcebda9d4d2105830eed
Kirill Nazarov <nazarov_kv@mail.ru>
2021-08-01 15:54:23 +0300

    Add todos for http client

--- src/fucking-great-advisor/http_client/client.go
+++ src/fucking-great-advisor/http_client/client.go
@@ -5,3 +5,15 @@ import "log"
 func DoSmth() {
 	log.Printf("Hi there from http clent!")
 }
+
+/*
+TODO:
+ - Use go http client to retrieve advice from https://fucking-great-advice.ru/api/random
+   Useful links:
+	* https://pkg.go.dev/net/http - Go Lang built-in http client
+ - Handle JSON response. Put it to Struct (do marshall).
+   Useful links:
+	* https://mholt.github.io/json-to-go/
+	* https://gobyexample.com/json
+	* https://habr.com/ru/post/502176/
+*/
